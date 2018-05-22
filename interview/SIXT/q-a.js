2faea8aef3114c6b29d03f2ed49f969bb384f79a
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-05-22 08:50:25 +0530

    Create q-a.js

---
+++ interview/SIXT/q-a.js
@@ -0,0 +1,6 @@
+Q-A: (your file: q-a.js)
+Suppose we have 2 arrays:
+var a = [10, 2, 3, 42, 5, 6];
+var b = ['f', 'b', 'c', 3, 'd', 'e'];
+How would you find out the element that is contained in both arrays in the most optimal way you can think of? Write the code in
+plain JavaScript (don't include any external lib like jQuery or whatever).
