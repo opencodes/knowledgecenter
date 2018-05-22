092a37a54a929e8a07e36b7bcf38eba6fd707be5
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-05-22 08:52:56 +0530

    Create q-c.js

---
+++ interview/SIXT/q-c.js
@@ -0,0 +1,6 @@
+/*Q-C: (your file: q-c.js)
+Imagine an application that utilizes an iframe where another application from another domain is loaded. How would you implement
+communication between those 2 applications to pass data from one application to the other. Explain it first and what problems might
+occur. Then propose your solution and show an example on how you would implement it – and explain why you did chose that
+option
+*/
