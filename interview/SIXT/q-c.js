b8eae440adae7a29163d8f6ef869aab73f742f98
Rajesh Kumar Jha <rkjha.it.in@gmail.com>
2018-05-24 12:35:47 +0530

    Update q-c.js

--- interview/SIXT/q-c.js
+++ interview/SIXT/q-c.js
@@ -12,7 +12,7 @@ document.getElementById('my-btn').addEventListener('click', function (e) {
 
 // For Cross window put origin * in second parameter
 document.getElementById('my-btn2').addEventListener('click', function (e) {
-    iframe_id_2.contentWindow.postMessage({ 'message': 'Please listen to window' }, 'http://localhost');
+    iframe_id_2.contentWindow.postMessage({ 'message': 'Please listen to window' }, '*');
 }, false);
 
 
